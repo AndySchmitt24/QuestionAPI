c3f4c474eed00a596b1b359e8fbe58591e01c669
Andy <andyschmitt24@gmail.com>
2023-09-25 12:28:19 +0200

    Alles Installiert

--- question.js
+++ question.js
@@ -3,3 +3,17 @@ const { v4: uuidv4 } = require(`uuid`);
 const bodyParser = require(`body-parser`);
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: `All Questions` });
+});
+
+app.get("/movies", (req, res) => {
+  res.json({ message: `im the movies route!` });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
